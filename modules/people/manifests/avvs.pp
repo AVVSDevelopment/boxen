59ab592ba94229ac231f721144ee09b4caa324f6
Vitaly Aminev <AVVS@users.noreply.github.com>
2015-10-13 01:32:42 +0300

    Create avvs.pp

---
+++ modules/people/manifests/avvs.pp
@@ -0,0 +1,8 @@
+# Set the global default node (auto-installs it if it can)
+class { 'nodejs::global':
+  version => 'iojs'
+}
+
+# install a node version
+nodejs::version { '0.12': }
+nodejs::version { '4': }
