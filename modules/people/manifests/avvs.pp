32c10908a49ef78e5e6815deddd819a9b3769705
Vitaly Aminev <AVVS@users.noreply.github.com>
2015-10-13 20:10:06 +0300

    Update avvs.pp

--- modules/people/manifests/avvs.pp
+++ modules/people/manifests/avvs.pp
@@ -1,4 +1,5 @@
 class people::avvs {
+  include atom
 
   # Set the global default node (auto-installs it if it can)
   class { 'nodejs::global':
@@ -8,5 +9,11 @@ class people::avvs {
   # install a node version
   nodejs::version { '0.12': }
   nodejs::version { '4': }
+
+  # install the linter package
+  atom::package { 'linter': }
+
+  # install the monokai theme
+  atom::theme { 'monokai': }
 
 }
