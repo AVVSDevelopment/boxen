39465136bae27b900b738cd1712983b2b02bb4aa
Vitaly Aminev <AVVS@users.noreply.github.com>
2015-12-20 19:10:09 +0300

    Update avvs.pp

--- modules/people/manifests/avvs.pp
+++ modules/people/manifests/avvs.pp
@@ -7,7 +7,15 @@ class people::avvs {
   include spotify
   
   # deprecated // use dockertoolbox
-  # include docker
+  include docker
+
+  # Keep Docker up-to-date
+  docker::compose::version: "1.5.2"
+  docker::engine::version: "1.9.1"
+  docker::machine::version: "0.5.4"
+  
+  # The name of the local VM
+  docker::machinename: "local"
 
   # Set the global default node (auto-installs it if it can)
   class { 'nodejs::global':
